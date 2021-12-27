90c7aa8403acaa97e86002e9f165d9069465258b
N0chteil <53608074+N0chteil@users.noreply.github.com>
2021-12-27 04:10:01 +0100

    Remove function

--- managers/app.js
+++ managers/app.js
@@ -189,20 +189,3 @@ app.restart = () => {
     app.relaunch();
     app.exit();
 }
-
-function isEqual(obj1, obj2) {
-    const obj1Keys = Object.keys(obj1),
-        obj2Keys = Object.keys(obj2);
-
-    if (obj1Keys.length !== obj2Keys.length) return false;
-
-    for (let objKey of obj1Keys) {
-        if (obj1[objKey] !== obj2[objKey]) {
-            if (typeof obj1[objKey] == "object" && typeof obj2[objKey] == "object")
-                if (!isEqual(obj1[objKey], obj2[objKey])) return false;
-            else return false;
-        }
-    }
-
-    return true;
-}
