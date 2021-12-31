a311f520322cfd1a7d4e29b3b5ab4f2d4195f0e3
N0chteil <53608074+N0chteil@users.noreply.github.com>
2021-12-31 19:51:21 +0100

    Fix iTunes

--- managers/itunes.js
+++ managers/itunes.js
@@ -1,9 +1,12 @@
-const iTunes = require("itunes-bridge"),
+const { app } = require("electron"),
+    iTunes = require("itunes-bridge"),
     iTunesEmitter = iTunes.emitter,
     Store = require("electron-store"),
-    { connect, updateActivity, clearActivity } = require("../managers/discord.js"),
+    { connect, updateActivity, clearActivity, replaceRPCVars } = require("../managers/discord.js"),
     config = new Store({}),
     appData = new Store({name: "data"});
+
+console.log = app.addLog;
 
 iTunesEmitter.on("playing", (type, currentTrack) => {
     console.log("[iTunes] Playing");
@@ -19,49 +22,42 @@ iTunesEmitter.on("paused", (type, currentTrack) => {
     clearActivity();
 });
 
-iTunesEmitter.on("timeChange", (type, currentTrack) => {
-    console.log("[iTunes] Time change");
-    if (!currentTrack) return console.log("[iTunes] No Track detected");
+iTunesEmitter.on("timeChange", async (type, currentTrack) => {
+    if (!app.discord.presenceData.details || app.discord.presenceData.details.length > 128) return;
+    let ct = Math.floor(Date.now() / 1000) - currentTrack.elapsedTime + currentTrack.duration;
 
-    updateActivity(type, currentTrack, "iTunes");
+    if (app.discord.presenceData.isLive) {
+        let ctg = Math.floor(Date.now() / 1000) - app.discord.rpc.ctG.elapsedTime + app.discord.rpc.ctG.duration;
+        const difference = (ct > ctg) ? ct - ctg : ctg - ct;
+
+        if (app.discord.presenceData.endTimestamp) delete app.discord.presenceData.endTimestamp;
+
+        if (difference > 99 && currentTrack.duration > 0) {
+            replaceRPCVars(currentTrack, "rpcDetails");
+            replaceRPCVars(currentTrack, "rpcState");
+
+            app.discord.presenceData.endTimestamp = Math.floor(Date.now() / 1000) - currentTrack.elapsedTime + (currentTrack.duration + (config.get("performanceMode") ? 1.75 : 1));
+            app.discord.presenceData.isLive = false;
+
+            if (app.discord.presenceData.isReady) app.discord.client.setActivity(app.discord.presenceData);
+        }
+        return;
+    }
+
+    if (!app.discord.presenceData.endTimestamp) return;
+
+    if (ct !== app.discord.presenceData.endTimestamp) {
+        const difference = (ct > app.discord.presenceData.endTimestamp) ? ct - app.discord.presenceData.endTimestamp : app.discord.presenceData.endTimestamp - ct;
+
+        if (difference > 1.5) {
+            app.discord.presenceData.endTimestamp = Math.floor(Date.now() / 1000) - currentTrack.elapsedTime + (currentTrack.duration + (config.get("performanceMode") ? 1.75 : 1));
+
+            if (app.discord.presenceData.isReady) app.discord.client.setActivity(app.discord.presenceData);
+        }
+    }
 });
 
 iTunesEmitter.on("stopped", () => {
     console.log("[iTunes] Stopped");
     clearActivity(true);
 });
-
-// iTunesEmitter.on("timeChange", async (type, currentTrack) => {
-//     if (!rpc.presenceData.details || rpc.presenceData.details.length > 128) return;
-//     let ct = Math.floor(Date.now() / 1000) - currentTrack.elapsedTime + currentTrack.duration;
-
-//     if (rpc.presenceData.isLive) {
-//         let ctg = Math.floor(Date.now() / 1000) - rpc.ctG.elapsedTime + rpc.ctG.duration;
-//         const difference = (ct > ctg) ? ct - ctg : ctg - ct;
-
-//         if (rpc.presenceData.endTimestamp) delete rpc.presenceData.endTimestamp;
-
-//         if (difference > 99 && currentTrack.duration > 0) {
-//             rpc.replaceRPCVars(currentTrack, "rpcDetails");
-//             rpc.replaceRPCVars(currentTrack, "rpcState");
-
-//             rpc.presenceData.endTimestamp = Math.floor(Date.now() / 1000) - currentTrack.elapsedTime + (currentTrack.duration + (config.get("performanceMode") ? 1.75 : 1));
-//             rpc.presenceData.isLive = false;
-
-//             if (rpc.presenceData.isReady) rpc.rpc.setActivity(rpc.presenceData);
-//         }
-//         return;
-//     }
-
-//     if (!rpc.presenceData.endTimestamp) return;
-
-//     if (ct !== rpc.presenceData.endTimestamp) {
-//         const difference = (ct > rpc.presenceData.endTimestamp) ? ct - rpc.presenceData.endTimestamp : rpc.presenceData.endTimestamp - ct;
-
-//         if (difference > 1.5) {
-//             rpc.presenceData.endTimestamp = Math.floor(Date.now() / 1000) - currentTrack.elapsedTime + (currentTrack.duration + (config.get("performanceMode") ? 1.75 : 1));
-
-//             if (rpc.presenceData.isReady) rpc.rpc.setActivity(rpc.presenceData);
-//         }
-//     }
-// });
